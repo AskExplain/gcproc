4ee23cffbd2ebea08ed73cac045060c080d22d05
AskExplain <ask@explain.group>
2021-06-04 17:13:51 +1000

    Adding main functions
    Includes updates to:
    main gcproc function
    prepare data function
    utils function

---
+++ R/prepare_data.R
@@ -0,0 +1,24 @@
+prepare_data <- function(x, y, log = F, center = T, scale.z = F){
+
+  n = dim(x)[1]
+
+  x <- as.matrix(x)
+  y <- as.matrix(y)
+
+  # Prepare for log_e(1+x) transform and library normalisation
+  cx <- Matrix::colSums(x)
+  cy <- Matrix::colSums(y)
+
+  if (log == T){
+    x <- log(1+x)
+    y <- log(1+y)
+  }
+  if (center == T | scale.z == T){
+
+    x <- scale(x,center = center,scale = scale.z)
+    y <- scale(y,center = center,scale = scale.z)
+
+  }
+
+  return(list(x=x,y=y))
+}
