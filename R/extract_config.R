41ca04c9b2c345c45218ecfe976f973a63bd2644
DavidB-XI <80179887+DavidB-XI@users.noreply.github.com>
2021-08-29 12:16:36 +1000

    Update extract_config.R
    Changed documentation of init option "svd" to "random"

--- R/extract_config.R
+++ R/extract_config.R
@@ -5,7 +5,7 @@
 #' @param max_iter Maximum iteration of gcproc
 #' @param tol Tolerance threshold for convergence (metric: Root Mean Squared Error)
 #' @param verbose Print statements?
-#' @param init Initialisation method for the model ("svd","eigen-quick","eigen-dense","svd-quick","svd-dense")
+#' @param init Initialisation method for the model ("random","eigen-quick","eigen-dense","svd-quick","svd-dense")
 #' @return  Configuration parameters for gcproc
 #' @export
 extract_config <- function(verbose=T){
