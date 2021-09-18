88b486291263eda5591394ef69bf101dccc629b1
AskExplain <david@explain.group>
2021-09-18 10:35:42 +1000

    Update recover
    Attempt to prevent NA in scaling for regression

--- R/recover_points.R
+++ R/recover_points.R
@@ -29,8 +29,8 @@ recover_points <- function(x,
 
     if (!is.null(recover$y)){
 
-      Y.y <- scale(y)%*%main.parameters$v.beta
-      X.x <- scale(x)%*%main.parameters$u.beta
+      Y.y <- ((y - colMeans(y))/apply(y,2,function(Y){Y+1e-3}))%*%main.parameters$v.beta
+      X.x <- ((x - colMeans(x))/apply(x,2,function(X){X+1e-3}))%*%main.parameters$u.beta
       Y.X <- Y.y + X.x
 
       y[,which((colSums(recover$y)>0)==T)]  <- do.call('cbind',lapply(X = c(which((colSums(recover$y)>0)==T)), FUN = function(id_col){
@@ -89,8 +89,8 @@ recover_points <- function(x,
 
     if (!is.null(recover$x)){
 
-      Y.y <- scale(y)%*%main.parameters$v.beta
-      X.x <- scale(x)%*%main.parameters$u.beta
+      Y.y <- ((y - colMeans(y))/apply(y,2,function(Y){Y+1e-3}))%*%main.parameters$v.beta
+      X.x <- ((x - colMeans(x))/apply(x,2,function(X){X+1e-3}))%*%main.parameters$u.beta
       Y.X <- Y.y + X.x
 
       x[,which((colSums(recover$x)>0)==T)]  <- do.call('cbind',lapply(X = c(which((colSums(recover$x)>0)==T)),FUN = function(id_col){
