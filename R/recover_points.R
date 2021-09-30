6dbcae1003612d3f135d3fea457aa4ba414927ec
AskExplain <david@explain.group>
2021-09-30 18:05:14 +1000

    Update recover_points.R
    Running recovery checks

--- R/recover_points.R
+++ R/recover_points.R
@@ -26,15 +26,22 @@ recover_points <- function(data_list,
       x <- as.matrix(data_list[[i]])
 
       if (!is.null(recover$design.list[[i]])){
+        print(c("check pre covariate",i))
 
         recover$covariate <- transform.data(Reduce("+",lapply(c(1:length(data_list)),function(X){
           transformed.data <- (t(main.parameters[[i]]$alpha)%*%MASS::ginv((main.parameters[[X]]$alpha)%*%t(main.parameters[[X]]$alpha))%*%(main.parameters[[X]]$alpha)%*%as.matrix(data_list[[X]])%*%(main.parameters[[X]]$beta)%*%MASS::ginv(t(main.parameters[[X]]$beta)%*%(main.parameters[[X]]$beta)))
         })))
 
+        print(c("check post covariate",i))
+
         x[,which((colSums(recover$design.list[[i]])>0)==T)]  <- do.call('cbind',lapply(X = c(which((colSums(recover$design.list[[i]])>0)==T)),FUN = function(id_col){
+
+          print(c("check pre design",i))
 
           test_id.x <- as.logical(recover$design.list[[i]][,id_col])
           train_id.x <- as.logical(1 - recover$design.list[[i]][,id_col])
+
+          print(c("check post design",i))
 
           if (min(x)==0){
             sparse.x <- log(x[,id_col]+1)
@@ -43,6 +50,8 @@ recover_points <- function(data_list,
             sparse.x <- x[,id_col]
             to_exp <- F
           }
+
+          print(c("check pre predict inner",i))
 
           if (any(test_id.x) & any(train_id.x)){
 
@@ -74,6 +83,7 @@ recover_points <- function(data_list,
               sparse.x[test_id.x] <- recover$fn(train = covariate_predictors, test = test_predictors, y = sparse.x[train_id.x], parameters = recover$param)
             }
 
+            print(c("check post predict inner",i))
 
           }
           return(if(to_exp){exp(sparse.x)-1}else{sparse.x})
