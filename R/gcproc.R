7aa7dc26ffb6419010b199be1fff7ea2a5124f2a
AskExplain <david@explain.group>
2021-10-02 10:07:37 +1000

    Update gcproc.R
    Provide print out of cores and batches

--- R/gcproc.R
+++ R/gcproc.R
@@ -48,7 +48,7 @@ gcproc <- function(data_list,
   }
 
   if (config$verbose){
-    print(paste("Beginning gcproc learning with:    Sample dimension reduction (config$i_dim): ",config$i_dim, "    Feature dimension reduction (config$j_dim): ", config$j_dim,"    Tolerance Threshold: ", config$tol, "   Maximum number of iterations: ", config$max_iter, "   Verbose: ", config$verbose,sep=""))
+    print(paste("Beginning gcproc learning with:    Sample dimension reduction (config$i_dim): ",config$i_dim, "    Feature dimension reduction (config$j_dim): ", config$j_dim,"    Tolerance Threshold: ", config$tol, "   Maximum number of iterations: ", config$max_iter, "   Verbose: ", config$verbose, "   Cores: ", config$n_cores, "   Batches: ", config$batch, sep=""))
   }
 
   while (T){
