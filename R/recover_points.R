8c9d816ab20debe68b3e74cb253c47c107643944
DavidB-XI <80179887+DavidB-XI@users.noreply.github.com>
2021-11-04 21:52:13 +1000

    Update recover_points.R

--- R/recover_points.R
+++ R/recover_points.R
@@ -30,12 +30,11 @@ recover_points <- function(data_list,
           column_with_missing_points <- unique(missing_points[,2])
           
           main.data <- transform.data(as.matrix(data_list[[i]]), method = recover$link_function[1])
-          
+        
           pred.encode <- 0
           for (iter.id in c(1:length(data_list))){
-            code.projection <- (main.parameters$alpha[[join$alpha[iter.id]]]%*%data_list[[iter.id]]%*%main.parameters$beta[[join$beta[iter.id]]]) 
-            alpha.projection <- t(main.parameters$alpha[[join$alpha[i]]])%*%MASS::ginv((main.parameters$alpha[[join$alpha[iter.id]]])%*%t(main.parameters$alpha[[join$alpha[iter.id]]]))
-            pred.encode <- pred.encode + scale(alpha.projection%*%code.projection) / length(data_list)
+            code.projection <- (main.code$code)%*%t(main.parameters$beta[[join$beta[iter.id]]])%*%(main.parameters$beta[[join$beta[iter.id]]]) 
+            pred.encode <- pred.encode + scale(t(main.parameters$alpha[[join$alpha[i]]])%*%code.projection) / length(data_list)
           }
           
           pred.encode <- cbind(1,pred.encode)
