e26594bd2b6f44be87ce6c721631708f2cfc391c
DavidB-XI <80179887+DavidB-XI@users.noreply.github.com>
2021-11-20 14:37:00 +1000

    Update initialise.gcproc.R

--- R/initialise.gcproc.R
+++ R/initialise.gcproc.R
@@ -97,13 +97,13 @@ initialise.parameters <- function(x,config,transfer){
   param.beta <-   if (!is.null(transfer$main.parameters$beta)){
     transfer$main.parameters$beta
   } else if (config$init[1]=="random"){
-    array(x[sample(c(1:dim(x)[1]),size = config$j_dim,replace = T),sample(c(1:dim(x)[2]))][sample(c(1:config$j_dim)),sample(c(1:dim(x)[2]))],dim=c(dim(x)[2],config$j_dim))
+    array((x[sample(c(1:dim(x)[1]),size = config$j_dim,replace = T),sample(c(1:dim(x)[2]))]+rnorm(dim(x)[2]))[sample(c(1:config$j_dim)),sample(c(1:dim(x)[2]))],dim=c(dim(x)[2],config$j_dim))
   } 
   
   param.alpha <- if (!is.null(transfer$main.parameters$alpha)){
     transfer$main.parameters$alpha
   } else if (config$init[1]=="random") {
-    array(x[sample(c(1:dim(x)[1])),sample(c(1:dim(x)[2]),size = config$i_dim,replace = T)][sample(c(1:dim(x)[1])),sample(c(1:config$i_dim))],dim=c(config$i_dim,dim(x)[1]))
+    array((x[sample(c(1:dim(x)[1])),sample(c(1:dim(x)[2]),size = config$i_dim,replace = T)]+rnorm(dim(x)[1]))[sample(c(1:dim(x)[1])),sample(c(1:config$i_dim))],dim=c(config$i_dim,dim(x)[1]))
   }
   
   pivots <- list(
